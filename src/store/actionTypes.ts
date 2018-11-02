13aa613ee6ec5b79ed5e748e1608f765af40fbad
Diamondyuan <admin@diamondyuan.com>
2018-11-03 00:33:35 +0800

    feat: 增加一些未来会使用的 actionType

--- src/store/actionTypes.ts
+++ src/store/actionTypes.ts
@@ -1,15 +1,33 @@
-
 const COUNTER = {
   ADD: 'COUNTER_ADD',
   MINUS: 'COUNTER_MINUS',
-  ADD_ASYNC: 'ADD_ASYNC'
+  ADD_ASYNC: 'COUNTER_ADD_ASYNC'
 };
 
 const ROUTER = {
-  SWITCH_TAB: 'router_SWITCH_TAB'
+  /** 退出登录 返回初始页面 */
+  LOGOUT: 'LOGOUT',
+  /** 切换 Tab */
+  SWITCH_TAB: 'ROUTER_SWITCH_TAB'
+};
+
+const USER = {
+  /** 请求用户信息 */
+  REFRESH_USER_INFO_REQUEST: 'USER_REFRESH_USER_INFO_REQUEST',
+  /** 刷新用户信息 */
+  REFRESH_USER_INFO: 'USER_REFRESH_USER_INFO'
+};
+
+const DOC = {
+  /** 获取用户文档列表 */
+  FETCH_CREATED_DOC_REQUEST: 'DOC_FETCH_CREATED_DOC_REQUEST',
+  /** 刷新用户创建的文档列表 */
+  INIT_CREATED_DOC_LIST: 'DOC_INIT_CREATED_DOC_LIST'
 };
 
 export default {
   COUNTER,
-  ROUTER
+  ROUTER,
+  USER,
+  DOC
 };
