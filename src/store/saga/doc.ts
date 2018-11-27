bbc43c75e1bd7ef67a071ce6ed26da94e64e3aaf
Diamondyuan <admin@diamondyuan.com>
2018-11-27 19:47:12 +0800

    refactor: 使用 action 的 type 替代原先直接从 actionTypes 取 type 的方式

--- src/store/saga/doc.ts
+++ src/store/saga/doc.ts
@@ -1,3 +1,4 @@
+import Taro from '@tarojs/taro';
 import { isType } from 'ts-action';
 import {
   takeLatest,
@@ -7,9 +8,9 @@ import {
   select,
   takeEvery
 } from 'redux-saga/effects';
-import actionTypes from '../actionTypes';
 import { getUserDocs, getDocDetail, deleteDocument } from '../../services/api';
 import {
+  createdDocumentPulldownRefreshRequest,
   initCreatedDocListSuccess,
   initCreatedDocListError,
   fetchDocumentDetailSuccess,
@@ -17,19 +18,17 @@ import {
   fetchDocumentDetailRequest,
   deleteDocumentRequest,
   deleteDocumentSuccess,
+  fetchMoreDocRequest,
   fetchMoreDocSuccess,
-  fetchMoreDocEnd
+  fetchMoreDocEnd,
+  initCreatedDocListRequest
 } from '../actions/doc';
-
 import { detailRouterBack } from '../actions/router';
-import Taro from '@tarojs/taro';
-
-const { DOC } = actionTypes;
 
 export function* fetchDocumentDetailRequestHandler() {
   while (true) {
     try {
-      const action = yield take(DOC.FETCH_DOCUMENT_DETAIL_REQUEST);
+      const action = yield take(fetchDocumentDetailRequest.type);
       if (isType(action, fetchDocumentDetailRequest)) {
         const { id, repoId } = action.payload;
         const detail = yield getDocDetail(repoId, id);
@@ -42,7 +41,7 @@ export function* fetchDocumentDetailRequestHandler() {
 }
 
 function* initCreatedDocListSages() {
-  yield takeLatest(DOC.INIT_CREATED_DOC_LIST_REQUEST, function* () {
+  yield takeLatest(initCreatedDocListRequest.type, function* () {
     try {
       const response = yield getUserDocs({ offset: 0 });
       yield put(initCreatedDocListSuccess({ createdDocs: response.data }));
@@ -53,7 +52,7 @@ function* initCreatedDocListSages() {
 }
 
 function* createdDocumentPulldownRefreshRequestSage() {
-  yield takeLatest(DOC.CREATED_DOCUMENT_PULL_DOWN_REFRESH_REQUEST, function* () {
+  yield takeLatest(createdDocumentPulldownRefreshRequest.type, function* () {
     try {
       const response = yield getUserDocs({ offset: 0 });
       yield put(initCreatedDocListSuccess({ createdDocs: response.data }));
@@ -69,7 +68,7 @@ function* createdDocumentPulldownRefreshRequestSage() {
 }
 
 function* fetchMoreDocRequestSaga() {
-  yield takeLatest(DOC.FETCH_MORE_DOC_REQUEST, function* () {
+  yield takeLatest(fetchMoreDocRequest.type, function* () {
     const store: GlobalStateInterface = yield select();
     const docCount = store.doc.createdDocs.length;
     const response = yield getUserDocs({ offset: docCount });
@@ -80,8 +79,9 @@ function* fetchMoreDocRequestSaga() {
     }
   });
 }
-function* deleteDocumentRequestSafa() {
-  yield takeEvery(DOC.DELETE_DOCUMENT_REQUEST, function* (action) {
+
+function* deleteDocumentRequestSaga() {
+  yield takeEvery(deleteDocumentRequest.type, function* (action) {
     if (isType(action, deleteDocumentRequest)) {
       try {
         yield deleteDocument(action.payload.repoId, action.payload.id);
@@ -98,7 +98,7 @@ function* deleteDocumentRequestSafa() {
 }
 
 export function* docRootSaga() {
-  yield fork(deleteDocumentRequestSafa);
+  yield fork(deleteDocumentRequestSaga);
   yield fork(initCreatedDocListSages);
   yield fork(fetchMoreDocRequestSaga);
   yield fork(createdDocumentPulldownRefreshRequestSage);
